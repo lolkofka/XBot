72fa725fb5e4163cdf0242014d8c24e36a032cb3
kewldan <kewldanil1@gmail.com>
2024-02-24 22:08:34 +0300

    v1.0.9

--- src/kwldn_bot/utils.py
+++ src/kwldn_bot/utils.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Union
 
 from aiogram import Bot
+from aiogram.exceptions import AiogramError
 from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
@@ -11,13 +12,20 @@ def get_timestamp(time: int) -> str:
     return datetime.fromtimestamp(time + 3600 * 3).strftime("%H:%M %d.%m.%Y")
 
 
+async def notify(bot: Bot, user_id: int, text: str):
+    try:
+        await bot.send_message(user_id, text)
+    except AiogramError:
+        pass
+
+
 async def distribute(bot: Bot, ids: list[int], text: str, additional_tasks: list[...] | None = None) -> tuple[...]:
     if additional_tasks is None:
         additional_tasks = []
 
     tasks = additional_tasks
     for user_id in ids:
-        tasks.append(bot.send_message(user_id, text))
+        tasks.append(notify(bot, user_id, text))
     return await asyncio.gather(*tasks)
 
 
