f71aa8143789a5e6f76f45b1d610cd3ccc337dc9
kewldan <kewldanil1@gmail.com>
2024-03-03 00:22:50 +0300

    v1.2.0

--- src/kwldn_bot/config.py
+++ src/kwldn_bot/config.py
@@ -1,4 +1,3 @@
-import collections
 import json
 import os.path
 from typing import TypeVar
@@ -6,16 +5,6 @@ from typing import TypeVar
 from pydantic import BaseModel
 
 config_file = 'data/config.json'
-
-
-def update(d, u):
-    for k, v in u.items():
-        if isinstance(v, collections.abc.Mapping):
-            d[k] = update(d.get(k, {}), v)
-        else:
-            d[k] = v
-    return d
-
 
 if not os.path.exists('data'):
     os.mkdir('data')
@@ -32,18 +21,15 @@ class BotSettings(BaseModel):
 class BasicBotConfig(BaseModel):
     bot: BotSettings = BotSettings()
 
+    def __init__(self):
+        if os.path.exists(config_file):
+            with open(config_file, 'r', encoding='utf-8') as f:
+                super().__init__(**json.load(f))
+        else:
+            super().__init__()
+
+        with open(config_file, 'w', encoding='utf-8') as f:
+            json.dump(self.model_dump(), f, ensure_ascii=True, sort_keys=True, indent=4)
+
 
 R = TypeVar('R')
-
-
-def load_config(config_type: R) -> R:
-    if os.path.exists(config_file):
-        with open(config_file, 'r', encoding='utf-8') as f:
-            bot_config = config_type(**json.load(f))
-    else:
-        bot_config = config_type()
-
-    with open(config_file, 'w', encoding='utf-8') as f:
-        json.dump(bot_config.model_dump(), f, ensure_ascii=True, sort_keys=True, indent=4)
-
-    return bot_config
