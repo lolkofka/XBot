a7d2926ea07069b9156eb015c3fd7fac8d4fed78
kewldan <kewldanil1@gmail.com>
2024-06-05 17:20:47 +0300

    v1.3.6

--- src/kwldn_bot/database/types.py
+++ src/kwldn_bot/database/types.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
 from beanie import Document
+from pydantic import Field
 
 
 class BaseUser(Document):
     user_id: int
     username: str | None
-    joined: datetime
+    joined: datetime = Field(default_factory=datetime.now)
