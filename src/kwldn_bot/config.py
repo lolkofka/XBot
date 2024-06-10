20d45f862a6835cdcdb0a7e6de993fd81f9f34e8
kewldan <kewldanil1@gmail.com>
2024-06-10 21:45:26 +0300

    v1.3.7

--- src/kwldn_bot/config.py
+++ src/kwldn_bot/config.py
@@ -3,8 +3,6 @@ import os.path
 
 from pydantic import BaseModel
 
-config_file = 'data/config.json'
-
 if not os.path.exists('data'):
     os.mkdir('data')
 
@@ -21,11 +19,14 @@ class BasicBotConfig(BaseModel):
     bot: BotSettings = BotSettings()
 
     def __init__(self):
-        if os.path.exists(config_file):
-            with open(config_file, 'r', encoding='utf-8') as f:
+        if os.path.exists(self.get_pathname()):
+            with open(self.get_pathname(), 'r', encoding='utf-8') as f:
                 super().__init__(**json.load(f))
         else:
             super().__init__()
 
-        with open(config_file, 'w', encoding='utf-8') as f:
+        with open(self.get_pathname(), 'w', encoding='utf-8') as f:
             json.dump(self.model_dump(), f, ensure_ascii=True, sort_keys=True, indent=4)
+
+    def get_pathname(self) -> str:
+        return 'data/config.json'
